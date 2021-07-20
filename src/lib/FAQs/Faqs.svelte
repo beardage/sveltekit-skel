8d50452f53765f493bfe07420e43ab4013b32002
beardage <xypress@gmail.com>
2021-07-19 20:27:22 -0700

    removing some more generic content

--- src/lib/FAQs/Faqs.svelte
+++ src/lib/FAQs/Faqs.svelte
@@ -1,22 +1,18 @@
 <script>
 	import Faq from '$lib/FAQs/Faq.svelte';
-	
+
 	let faqItems = [
-		{ 
-			question: "What is the difference between a misdemeanor and a felony?",
-			answer: "About 10 years"
+		{
+			question: 'Question?',
+			answer: 'Answer'
 		},
 		{
-			question: "Do I have to testify in court?",
-			answer: "No you do not, if doing so will disadvantage you."
+			question: 'Question?',
+			answer: 'Answer'
 		},
 		{
-			question: "I have been served with a search warrant, what should I do?",
-			answer: "You must immediately comply with the warrant, but also call legal counsel immediately."
-		},
-		{
-			question: "How do I establish a good defense strategy for my case?",
-			answer: "Find yourself a good attorney like KraussLaw."
+			question: 'Question?',
+			answer: 'Answer'
 		}
 	];
 </script>
